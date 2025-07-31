6f799af0537995379a17b960f1436f7b7f0c64bf
rahul9852-dot <kumarrahul31199@gmail.com>
2025-07-31 17:08:05 +0530

    test fix

--- apps/jobber-auth/src/app/users/users.service.spec.ts
+++ apps/jobber-auth/src/app/users/users.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: PrismaService,
+          useValue: {
+            user: {},
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
